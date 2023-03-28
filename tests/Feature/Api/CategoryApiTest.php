a2da2a1066aa23fef19d1d88b31bfb824c9cee8a
Rodolfo Neto <rodolfoneto>
2023-03-28 14:50:54 -0700

    feat add: test e2e get category

--- tests/Feature/Api/CategoryApiTest.php
+++ tests/Feature/Api/CategoryApiTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Api;
 
 use App\Models\Category as ModelCategory;
+use Illuminate\Http\Response;
 use Tests\TestCase;
 
 class CategoryApiTest extends TestCase
@@ -19,9 +20,62 @@ class CategoryApiTest extends TestCase
 
     public function testListAllCategories()
     {
-        $categories = ModelCategory::factory()->count(30)->create();
+        ModelCategory::factory()->count(30)->create();
 
         $response = $this->getJson($this->endpoint);
         $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'meta' => [
+                'total',
+                'current_page',
+                'last_page',
+                'first_page',
+                'per_page',
+                'to',
+                'from'
+            ],
+        ]);
+        $response->assertJsonCount(15, 'data');
+        $this->assertEquals(30, $response['meta']['total']);
+    }
+
+    public function testPaginateCategories()
+    {
+        $categories = ModelCategory::factory()->count(30)->create();
+
+        $response = $this->getJson("{$this->endpoint}?page=2");
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'meta' => [
+                'total',
+                'current_page',
+                'last_page',
+                'first_page',
+                'per_page',
+                'to',
+                'from'
+            ],
+        ]);
+        $response->assertJsonCount(15, 'data');
+        $this->assertEquals($categories['15']->id, $response['data'][0]['id']);
+        $this->assertEquals(2, $response['meta']['current_page']);
+    }
+
+    public function testListCategoryNotFound()
+    {
+        $response = $this->getJson("{$this->endpoint}/fake_id");
+        $response->assertStatus(Response::HTTP_NOT_FOUND);
+    }
+
+    public function testListCategory()
+    {
+        $category = ModelCategory::factory()->create();
+
+        $response = $this->getJson("{$this->endpoint}/{$category->id}");
+        $response->assertJsonStructure([
+            'data' => ['id', 'name', 'description', 'is_active', 'created_at']
+        ]);
+        $this->assertEquals($category->id, $response['data']['id']);
+        $response->assertStatus(Response::HTTP_OK);
     }
 }
